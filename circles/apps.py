9ebf5bd01f4f90ef3351dd046adb09e36a97c37e
rhman <rhmanibrahimmuhammad@gmail.com>
2023-05-20 00:57:16 +0300

    included the signals.py in app config

--- circles/apps.py
+++ circles/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class CirclesConfig(AppConfig):
+
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'circles'
+    name               = 'circles'
+
+    def ready(self):
+        from . import signals
