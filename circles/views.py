14ae35395e33b110add2ed0ce0f3b53e58eff46f
rhman <rhmanibrahimmuhammad@gmail.com>
2023-05-20 01:05:32 +0300

    created the needed files, mapped templates

--- circles/views.py
+++ circles/views.py
@@ -6,5 +6,12 @@ from user.models import Account
 
 
 @authenticated(True)
+def form(request):
+    return render(request, "circles/form.html")
+
+@authenticated(True)
+def manage(request):
+    return render(request, "circles/manage.html")
+
 def index(request):
     return render(request, "circles/index.html")
