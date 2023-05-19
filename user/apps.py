3e7a76be13a5ed599d5e5306cf3dfeb60691d908
rhman <rhmanibrahimmuhammad@gmail.com>
2023-05-20 00:55:47 +0300

    added white spaces

--- user/apps.py
+++ user/apps.py
@@ -4,7 +4,7 @@ from django.apps import AppConfig
 class UserConfig(AppConfig):
 
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'user'
+    name               = 'user'
 
     def ready(self):
         from . import signals
