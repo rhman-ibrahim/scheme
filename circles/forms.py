8c5180e4fa84db8ae905d436c42a03c696cdb0c4
rhman <rhmanibrahimmuhammad@gmail.com>
2023-05-23 19:31:56 +0300

    the very beginning

--- circles/forms.py
+++ circles/forms.py
@@ -1,1 +1,27 @@
 from django import forms
+from .models import Circle
+
+
+class CircleForm(forms.ModelForm):
+
+    name = forms.CharField(
+        max_length=32,
+        required=True
+    )
+    description = forms.CharField(
+        max_length=512,
+        required=False,
+        widget=forms.Textarea(
+            attrs = {
+                'data-field':"description",
+                'placeholder': "(optional)",
+                'autocomplete':"off",
+                'maxlength':512,
+            }
+        )
+    )
+
+    class Meta:
+
+        model  = Circle
+        fields = ['name', 'description']
--- circles/urls.py
+++ circles/urls.py
@@ -5,5 +5,7 @@ from . import views
 app_name    = "circles"
 urlpatterns = [
     path('connect/', views.index, name="connect"),
+    path('create/', views.create, name="create"),
+    path('manage/', views.manage, name="manage"),
     path('<str:uuid>/', views.invitation, name="invitation"),
 ]
