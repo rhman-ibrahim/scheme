c381a8e45376aa2b5c4ad44ef53d14ce06d485c8
rhman <rhmanibrahimmuhammad@gmail.com>
2023-05-25 11:02:23 +0300

    updated authentication, created guest decorators

--- user/decorators.py
+++ user/decorators.py
@@ -2,34 +2,44 @@ from django.shortcuts import redirect
 from django.contrib import messages
 
 
-def superuser(view):
-    # Check if the user is a superuser.
-    def wrapper(request, *args, **kwargs):
-        if request.user.is_superuser:
-            return view(request, *args, **kwargs)
-        messages.warning(request, 'you are not authorized to view this')
-        return redirect("user:settings")
-    return wrapper
-
-def authenticated(status):
-    # Check if the user is authenticated.
-    def decorator(view):
-        def wrapper(request, *args, **kwargs):
-            if status == request.user.is_authenticated:
-                return view(request, *args, **kwargs)
-            if status:
-                messages.warning(request, "you have to signin first.")
-                return redirect('home:user')
-            messages.warning(request, "you have to signouty first.")
-            return redirect("user:settings")
-        return wrapper
-    return decorator
-
 def activated(view):
-    # Check if the user is activated.
     def wrapper(request, *args, **kwargs):
         if request.user.is_active:
             return view(request, *args, **kwargs)
         messages.warning(request, 'your account is not activated')
-        return redirect("user:settings")
+        return redirect("user:navigate")
     return wrapper
+
+def superuser(view):
+    def wrapper(request, *args, **kwargs):
+        if request.user.is_superuser:
+            return view(request, *args, **kwargs)
+        messages.warning(request, 'you are not authorized to view this')
+        return redirect("user:navigate")
+    return wrapper
+
+def authentication(status):
+    def decorator(view):
+        def wrapper(request, *args, **kwargs):
+            if status == request.user.is_authenticated:
+                return view(request, *args, **kwargs)
+            else:
+                if status:
+                    messages.warning(request, "you have to signin first.")
+                else:
+                    messages.warning(request, "you have to signout first.")
+                return redirect('user:navigate')
+        return wrapper
+    return decorator
+
+def guest(allowed):
+    def decorator(view):
+        def wrapper(request, *args, **kwargs):
+            if allowed:
+                if request.user.is_authenticated and request.user.is_lazy:
+                    return view(request, *args, **kwargs)
+                else:
+                    messages.warning(request, "This is a guest account, signup first.")
+            return redirect('user:navigate')
+        return wrapper
+    return decorator
