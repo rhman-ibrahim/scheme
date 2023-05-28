434e70e7139ec706789dc89f1e5afdcc2e105eee
rhman <rhmanibrahimmuhammad@gmail.com>
2023-05-28 10:47:38 +0300

    recoded the views and urls

--- circles/urls.py
+++ circles/urls.py
@@ -5,8 +5,10 @@ from . import views
 app_name    = "circles"
 urlpatterns = [
     path('create/', views.create, name="create"),
-    path('invite/', views.invite, name="invite"),
     path('manage/', views.manage, name="manage"),
-    path('<str:uuid>/', views.invitation, name="invitation"),
-    path('terminate/', views.terminate, name="terminate"),
+    path('ask/<str:uuid>/', views.ask, name="ask"),
+    path('open/<str:uuid>/', views.open, name="open"),
+    path('approve/<int:cid>/<int:uid>/', views.approve, name="approve"),
+    path('reject/<int:cid>/<int:uid>/', views.reject, name="reject"),
+    path('close/', views.close, name="close"),
 ]
--- circles/views.py
+++ circles/views.py
@@ -1,14 +1,42 @@
-from django.contrib.admin.models import LogEntry, ADDITION
-from django.contrib.contenttypes.models import ContentType
+# Django
 from django.shortcuts import render, redirect
+from django.contrib.admin.models import ADDITION, CHANGE
 from django.contrib import messages
-from helpers.decorators import resource
-from helpers.functions import get_form_errors, log
-from user.functions import create_a_guest_user
-from user.decorators import authentication
-from .models import Circle, Invitation
-from .forms import CircleForm, InvitationForm
 
+# Helpers
+from helpers.functions import get_form_errors, log
+from helpers.decorators import resource
+
+# User
+from user.functions import create_a_guest_user
+
+# Circles
+from .decorators import circle, founder, member
+from .forms import CircleForm
+from .models import Circle
+
+
+@member
+def manage(request):
+    circle            = Circle.objects.get(id=request.session.get('circle'))
+    if circle.founder == request.user:
+        return render(
+            request,
+            "circles/founder.html",
+            {
+                'circle': circle,
+                'forms': {
+                    'circle': CircleForm(instance=circle),
+                }
+            }
+        )
+    return render(
+            request,
+            "circles/member.html",
+            {
+                'circle': circle,
+            }
+        )
 
 def create(request):
     if  request.method == "POST":
@@ -19,11 +47,8 @@ def create(request):
             circle         = form.save(commit=False)
             circle.founder = request.user
             circle         = form.save()
-            log(
-                request.user.id,
-                circle,
-                ADDITION,
-                f"created the circle"
+            log(request.user.id, circle, ADDITION,
+                f"created the circle ({circle.name})."
             )
             request.session['circle'] = f'{circle.id}'
             messages.success(request, "your circle is created successfully")
@@ -31,59 +56,47 @@ def create(request):
             get_form_errors(request, form)
     return redirect("circles:manage")
 
-def invite(request):
-    if  request.method == "POST":
-        circle      = Circle.objects.get(id=request.session.get('circle'))
-        form        = InvitationForm(request.POST)
-        if form.is_valid():
-            invitation         = form.save(commit=False)
-            invitation.circle  = circle
-            invitation         = form.save()
-            log(
-                request.user.id,
-                circle,
-                ADDITION,
-                f"created an invitation for {invitation.limit} memebers"
-            )
-        else:
-            get_form_errors(request, form)
+def open(request, uuid):
+    circle = Circle.objects.get(uuid=uuid)
+    request.session['circle'] = circle.id
     return redirect("circles:manage")
 
-@authentication(True)
-def manage(request):
-    circle      = Circle.objects.get(id=request.session.get('circle'))
-    return render(
-        request,
-        "circles/manage.html",
-        {
-            'circle': circle,
-            'logs': LogEntry.objects.filter(
-                content_type = ContentType.objects.get_for_model(Circle),
-                object_id    = circle.id
-            ),
-            'invitations': Invitation.objects.filter(circle=circle),
-            'states': {
-                'description': True if circle.description else False,
-                'founder':True if circle.founder == request.user else False
-            },
-            'forms': {
-                'circle': CircleForm(instance=circle),
-                'invitation': InvitationForm
-            }
-        }
-    )
-
-@resource("home:circles")
-def invitation(request, uuid):
-    inv = Invitation.objects.get(uuid=uuid)
-    if not request.user.is_authenticated:
-        create_a_guest_user(request)
-    inv.invitees.add(request.user)
-    inv.circle.members.add(request.user)
-    inv.save()
-    inv.circle.save()
-    return redirect("circles:connect")
-
-def terminate(request):
+@circle
+def close(request):
     if request.session['circle']: del request.session['circle']
     return redirect("user:navigate")
+
+@resource("user:navigate")
+def ask(request, uuid):
+    circle = Circle.objects.get(uuid=uuid)
+    if not request.user.is_authenticated:
+        create_a_guest_user(request)
+    circle.requested.add(request.user)
+    circle.save()
+    log(request.user.id, circle, ADDITION,
+        f"requested to join the circle ({circle.name})."
+    )
+    return redirect("user:navigate")
+
+@founder
+def approve(request, cid, uid):
+    circle = Circle.objects.get(id=cid)
+    user   = circle.requested.get(id=uid)
+    circle.members.add(user)
+    circle.requested.remove(user)
+    circle.save()
+    log(request.user.id, circle, CHANGE,
+        f"approved ({user.username}) joining the circle ({circle.name})."
+    )
+    return redirect("circles:manage")
+
+@founder
+def reject(request, cid, uid):
+    circle = Circle.objects.get(id=cid)
+    user   = circle.requested.get(id=uid)
+    circle.requested.remove(user)
+    circle.save()
+    log(request.user.id, circle, CHANGE,
+        f"rejected ({user.username}) joining the circle ({circle.name})."
+    )
+    return redirect("circles:manage")
