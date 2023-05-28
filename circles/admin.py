1fa5312081be81efdddca9fc830c87d54fe3567f
rhman <rhmanibrahimmuhammad@gmail.com>
2023-05-28 10:46:42 +0300

    corrected Circle model registering

--- circles/admin.py
+++ circles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Circle, Invitation
+from .models import Circle, CircleRequests, CircleMembership
 
 admin.site.register(Circle)
-admin.site.register(Invitation)
+admin.site.register(CircleRequests)
+admin.site.register(CircleMembership)
