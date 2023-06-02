2d7a28bb38f0ab9585e719fcf684129cf3738d64
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-03 01:18:56 +0300

    changed commands order

--- rebuild.sh
+++ rebuild.sh
@@ -48,11 +48,11 @@ if sudo lsof -i :6379
 then sudo lsof -t -i :6379 | sudo xargs kill
 fi
 
-# 1 - Run Redis
-gnome-terminal --window --working-directory "$PWD" --title "Redis" -- bash -c "redis-server"
-
-# 2 - Run Django
+# 1 - Run Django
 gnome-terminal --window --working-directory "$PWD" --title "Django" -- bash -c "python manage.py runserver 0.0.0.0:8000"
+
+# 2 - Run Redis
+gnome-terminal --window --working-directory "$PWD" --title "Redis" -- bash -c "redis-server"
 
 # 3 - Run Celery Worker
 gnome-terminal --window --working-directory "$PWD" --title "Celery/Worker" -- bash -c "celery -A scheme worker -l info"
