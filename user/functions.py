866ef4c9673b8260b1c0ebc16b7bcfff4360958e
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-03 01:11:04 +0300

    separated user cards in dirs

--- user/functions.py
+++ user/functions.py
@@ -9,10 +9,24 @@ from user.models import Account
 
 
 def create_a_guest_user(request):
+
     uuid_key_8 = str(uuid.uuid4())[:8]
     uuid_key16 = str(uuid.uuid4())[:16]
-    Account.objects.create_lazy_user(username=uuid_key_8, password=uuid_key16)
-    user = authenticate(username=uuid_key_8, password=uuid_key16)
+    
+    Account.objects.create_lazy_user(
+        username=uuid_key_8,
+        password=uuid_key16
+    )
+    user = authenticate(
+        username=uuid_key_8,
+        password=uuid_key16
+    )
+    
     if user is not None:
         login(request, user)
-        log(request.user.id, request.user, CHANGE, "signed in as a user")
+        log(
+            request.user.id,
+            request.user,
+            CHANGE,
+            "signed in as a user"
+        )
