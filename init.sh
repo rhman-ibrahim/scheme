b0106ec0f69c347767a63fe3f95d1272d85af6a7
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-03 07:26:58 +0300

    used --tab to open tabs instead of windows

--- init.sh
+++ init.sh
@@ -6,13 +6,13 @@ then sudo lsof -t -i :6379 | sudo xargs kill
 fi
 
 # 1 - Run Django
-gnome-terminal --window --working-directory "$PWD" --title "Django" -- bash -c "python manage.py runserver 0.0.0.0:8000"
+gnome-terminal --tab --title "Django" -- bash -c "python manage.py runserver 0.0.0.0:8000"
 
 # 2 - Run Redis
-gnome-terminal --window --working-directory "$PWD" --title "Redis" -- bash -c "redis-server"
+gnome-terminal --tab --title "Redis" -- bash -c "redis-server"
 
 # 3 - Run Celery Worker
-gnome-terminal --window --working-directory "$PWD" --title "Celery/Worker" -- bash -c "celery -A scheme worker -l info"
+gnome-terminal --tab --title "Celery/Worker" -- bash -c "celery -A scheme worker -l info"
 
 # 4 - Run Celery Beat
-gnome-terminal --window --working-directory "$PWD" --title "Celery/Beat" -- bash -c "celery -A scheme beat -l info"
+gnome-terminal --tab --title "Celery/Beat" -- bash -c "celery -A scheme beat -l info"
