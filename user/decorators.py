17e8e5da0778f45267056dd6eba582ffbc537583
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-03 20:06:25 +0300

    rewrote user decorators

--- user/decorators.py
+++ user/decorators.py
@@ -2,44 +2,26 @@ from django.shortcuts import redirect
 from django.contrib import messages
 
 
-def activated(view):
-    def wrapper(request, *args, **kwargs):
-        if request.user.is_active:
-            return view(request, *args, **kwargs)
-        messages.warning(request, 'your account is not activated')
-        return redirect("user:navigate")
-    return wrapper
-
-def superuser(view):
-    def wrapper(request, *args, **kwargs):
-        if request.user.is_superuser:
-            return view(request, *args, **kwargs)
-        messages.warning(request, 'you are not authorized to view this')
-        return redirect("user:navigate")
-    return wrapper
-
-def authentication(status):
+def is_authenticated(status):
     def decorator(view):
         def wrapper(request, *args, **kwargs):
             if status == request.user.is_authenticated:
                 return view(request, *args, **kwargs)
             else:
-                if status:
-                    messages.warning(request, "you have to signin first.")
-                else:
-                    messages.warning(request, "you have to signout first.")
+                if status: messages.warning(request, "you have to signin first.")
+                messages.warning(request, "you have to signout first.")
                 return redirect('user:navigate')
         return wrapper
     return decorator
 
-def guest(allowed):
+def is_guest(status):
     def decorator(view):
         def wrapper(request, *args, **kwargs):
-            if allowed:
-                if request.user.is_authenticated and request.user.is_guest:
-                    return view(request, *args, **kwargs)
-                else:
-                    messages.warning(request, "This is a guest account, signup first.")
-            return redirect('user:navigate')
+            if status == request.user.is_guest:
+                return view(request, *args, **kwargs)
+            else:
+                if status: messages.warning(request, "Only guest users are allowed to view this.")
+                messages.warning(request, "Guest users are not allowed to view this.")
+                return redirect('user:navigate')
         return wrapper
     return decorator
