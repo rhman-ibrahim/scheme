7c8b0857952a6f5ae74f5fc2c5463e9905fa5966
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-12 17:22:18 +0300

    circles is not stable yet

---
+++ circles/decorators.py
@@ -0,0 +1,13 @@
+from django.contrib import messages
+from django.shortcuts import redirect
+from circles.processors import opened_circle
+
+
+def circle_session(view):
+    def decorator(request, *args, **kwargs):
+        if 'circle' in request.session:
+            return view(request, *args, **kwargs)
+        else:
+            messages.warning(request, "there is no opened circle")
+            return redirect('user:navigate')
+    return decorator
--- circles/processors.py
+++ circles/processors.py
@@ -1,8 +1,17 @@
 from circles.models import Circle
+from django.shortcuts import redirect
+from django.contrib import messages
 
 
 def opened_circle(request):
-    circle_uuid = request.session['circle'] if 'circle' in request.session else None
-    return {
-        'opened_circle': Circle.objects.get(uuid=circle_uuid) if circle_uuid else None
-    }
+        try:
+            session = True if 'circle' in request.session else False
+            serial  = request.session.get('circle') if session else None 
+            circle  = Circle.objects.get(uuid=serial) if serial else None
+        except Circle.DoesNotExist:
+            request.session.pop('circle')
+            messages.warning(request, "circle does not exsit")
+            return redirect('user:navigate')
+        return {
+            'opened_circle': circle
+        }
