59e268cc32ee2ac34fdf7049e48b9e1ed213b8b9
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-14 19:25:35 +0300

    configured ASGI to handle spaces/room

--- scheme/asgi.py
+++ scheme/asgi.py
@@ -1,7 +1,28 @@
 import os
-
+# Django
 from django.core.asgi import get_asgi_application
+from django.urls import path
+# Channels
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+# Spaces
+from spaces.consumers import ChatConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scheme.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scheme.settings")
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": get_asgi_application(),
+
+    # WebSocket chat handler
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            URLRouter([
+                path('spaces/<str:serial>/', ChatConsumer.as_asgi()),
+            ])
+        )
+    ),
+
+})
