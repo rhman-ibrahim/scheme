5a29fd6d9323210b7036b4f52fb0bdecf20f64cf
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-14 19:31:29 +0300

    created room form

---
+++ spaces/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from .models import Room
+
+class RoomForm(forms.ModelForm):
+
+    name = forms.CharField(
+        label='room name',
+        widget = forms.TextInput(
+            attrs = {
+                'id':'room-name',
+                'placeholder':'room name'
+            }
+        )
+    )
+
+    class Meta:
+        model   = Room
+        fields  = ['name', 'users', 'description']
+        widgets = {
+            'users': forms.CheckboxSelectMultiple(),
+            'description': forms.Textarea(
+                attrs={
+                    'placeholder':'What is this room for?'
+                }
+            )
+        }
