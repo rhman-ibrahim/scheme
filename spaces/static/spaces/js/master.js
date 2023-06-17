1d65c64c9aad33de0abbe967f6d9d96269b38068
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-17 14:00:48 +0300

    covered fetch call in event listener

--- spaces/static/spaces/js/master.js
+++ spaces/static/spaces/js/master.js
@@ -2,25 +2,6 @@ const SPACE         = document.querySelector('#space');
 const SOCKET        = new WebSocket(`ws://${window.location.host}/spaces/${SPACE.dataset.serial}/`);
 const BUTTON        = document.querySelector('#sendButton');
 
-fetch(`http://${window.location.host}/spaces/${SPACE.dataset.serial}/`)
-.then(response => response.json())
-.then(
-    data => {
-        const FRAGMENT = document.createDocumentFragment();
-        data.forEach(
-            message => {
-                Space.messageAppend(FRAGMENT, message);
-            }
-        );
-        SPACE.append(FRAGMENT);
-    }
-).catch(
-    error => {
-        console.log(error)
-    }
-)
-
-
 SOCKET.onerror = function(error) {
     console.error('WebSocket error:', error);
 };
@@ -38,13 +19,10 @@ BUTTON.onclick = e => {
 }
 
 SOCKET.onmessage = e => {
-
     const data = JSON.parse(e.data);
-    
     if (data.event === 'message') {
         Space.messageAppend(SPACE, data);
     }
-
     if (data.event === 'notify') {
         let notification = String(data.notification);
         if (!notification.includes(SPACE.dataset.username)) {
@@ -55,3 +33,22 @@ SOCKET.onmessage = e => {
         };
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(`http://${window.location.host}/spaces/${SPACE.dataset.serial}/`)
+    .then(response => response.json())
+    .then(
+        data => {
+            const FRAGMENT = document.createDocumentFragment();
+            data.forEach(
+                message => {
+                    Space.messageAppend(FRAGMENT, message);
+                }
+            );
+            SPACE.append(FRAGMENT);
+    }).catch(
+        error => {
+            console.log(error)
+        }
+    )
+})
