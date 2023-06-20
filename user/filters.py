da06fe5cb21ce71a879ee8c437f48722908e9e7d
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-20 14:33:31 +0300

    updated user filter, created new cards

--- user/filters.py
+++ user/filters.py
@@ -5,7 +5,6 @@ register = template.Library()
 
 @register.filter
 def profile_picture_url(user):
-    if user.is_authenticated:
-        if hasattr(user, 'profile') and user.profile.picture:
-            return user.profile.picture.url
+    if hasattr(user, 'profile') and user.profile.picture:
+        return user.profile.picture.url
     return settings.DEFAULT_PROFILE_PICTURE_URL
