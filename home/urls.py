a34710871bc34c948e365cdf9ee9a681226c5efb
rhman <rhmanibrahimmuhammad@gmail.com>
2023-06-24 08:41:38 +0300

    removed app index, included reset form and card

--- home/urls.py
+++ home/urls.py
@@ -5,10 +5,5 @@ from . import views
 app_name    = "home"
 urlpatterns = [
     path('', views.index, name="index"),
-    path('user/', views.user, name="user"),
-    path('circles/', views.circles, name="circles"),
-    path('spaces/', views.spaces, name="spaces"),
-    path('signals/', views.signals, name="signals"),
-    path('tasks/', views.tasks, name="tasks"),
     path('back/', views.back, name="back")
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,5 @@
 # Django
-from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
+from django.shortcuts import render
 # User
 from user.forms import (
     SignUpForm, SignInForm, VerifyForm, PassWordResetForm
@@ -10,75 +9,18 @@ from user.decorators import is_authenticated
 from circles.forms import CircleForm
 
 
-context = {
-    'signup': SignUpForm,
-    'signin': SignInForm,
-    'circle': CircleForm,
-    'verify': VerifyForm,
-    'reset': PassWordResetForm(False)
-}
-
 @is_authenticated(False)
 def index(request):
     return render(
         request,
         "home/index.html",
         {
-            'forms':context
+            'forms': {
+                'signup': SignUpForm,
+                'signin': SignInForm,
+                'circle': CircleForm,
+                'verify': VerifyForm,
+                'reset': PassWordResetForm(False)
+            }
         }
     )
-
-@is_authenticated(False)
-def user(request):
-    return render(
-        request,
-        "home/user.html",
-        {
-            'forms':context
-        }
-    )
-
-@is_authenticated(False)
-def circles(request):
-    return render(
-        request,
-        "home/circles.html",
-        {
-            'forms':context
-        }
-    )
-
-def signals(request):
-    if request.user.is_authenticated:
-        return redirect("signals:list")
-    
-    return render(
-        request,
-        "home/signals.html",
-        {
-            'forms':context
-        }
-    )
-
-@is_authenticated(False)
-def spaces(request):
-    return render(
-        request,
-        "home/spaces.html",
-        {
-            'forms':context
-        }
-    )
-
-@is_authenticated(False)
-def tasks(request):
-    return render(
-        request,
-        "home/tasks.html",
-        {
-            'forms':context
-        }
-    )
-
-def back(request):
-     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
