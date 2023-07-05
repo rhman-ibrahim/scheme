d64d3b83fdf72d155273b8a7a14ff67b9e6e4942
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-05 15:11:14 +0300

    renamed mates app to mate

--- mate/apps.py
+++ mate/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MatesConfig(AppConfig):
+class MateConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'mates'
+    name = 'mate'
