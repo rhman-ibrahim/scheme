5311f9d1890566c996047037982031ece019f328
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-06 12:49:54 +0300

    checked if the element is found

--- ping/static/ping/js/master.js
+++ ping/static/ping/js/master.js
@@ -1,54 +1,57 @@
-const SPACE         = document.querySelector('#space');
-const SOCKET        = new WebSocket(`ws://${window.location.host}/ping/${SPACE.dataset.serial}/`);
-const BUTTON        = document.querySelector('#sendButton');
+if (Template.isThere('#space')) {
 
-SOCKET.onerror = function(error) {
-    console.error('WebSocket error:', error);
-};
-
-BUTTON.onclick = e => {
-    SOCKET.send(
-        JSON.stringify(
-            {
-                'sender': String(SPACE.dataset.username),
-                'body': String(document.querySelector('#message').value)
+    const SPACE         = document.querySelector('#space');
+    const SOCKET        = new WebSocket(`ws://${window.location.host}/ping/${SPACE.dataset.serial}/`);
+    const BUTTON        = document.querySelector('#sendButton');
+    
+    SOCKET.onerror      = function(error) {
+        console.error('WebSocket error:', error);
+    };
+    
+    BUTTON.onclick      = e => {
+        SOCKET.send(
+            JSON.stringify(
+                {
+                    'sender': String(SPACE.dataset.username),
+                    'body': String(document.querySelector('#message').value)
+                }
+            )
+        );
+        document.querySelector('#message').value = '';
+    }
+    
+    SOCKET.onmessage    = e => {
+        const data      = JSON.parse(e.data);
+        if (data.event === 'message') {
+            Space.messageAppend(SPACE, data);
+        }
+        if (data.event === 'notify') {
+            let notification = String(data.notification);
+            if (!notification.includes(SPACE.dataset.username)) {
+                let li         = document.createElement('li');
+                li.textContent = `${notification}`;
+                li.classList.add('notification');
+                SPACE.appendChild(li);
+            };
+        }
+    }
+    
+    document.addEventListener('DOMContentLoaded', () => {
+        fetch(`http://${window.location.host}/ping/${SPACE.dataset.serial}/`)
+        .then(response => response.json())
+        .then(
+            data => {
+                const FRAGMENT = document.createDocumentFragment();
+                data.forEach(
+                    message => {
+                        Space.messageAppend(FRAGMENT, message);
+                    }
+                );
+                SPACE.append(FRAGMENT);
+        }).catch(
+            error => {
+                console.log(error)
             }
         )
-    );
-    document.querySelector('#message').value = '';
+    })
 }
-
-SOCKET.onmessage = e => {
-    const data = JSON.parse(e.data);
-    if (data.event === 'message') {
-        Space.messageAppend(SPACE, data);
-    }
-    if (data.event === 'notify') {
-        let notification = String(data.notification);
-        if (!notification.includes(SPACE.dataset.username)) {
-            let li         = document.createElement('li');
-            li.textContent = `${notification}`;
-            li.classList.add('notification');
-            SPACE.appendChild(li);
-        };
-    }
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetch(`http://${window.location.host}/ping/${SPACE.dataset.serial}/`)
-    .then(response => response.json())
-    .then(
-        data => {
-            const FRAGMENT = document.createDocumentFragment();
-            data.forEach(
-                message => {
-                    Space.messageAppend(FRAGMENT, message);
-                }
-            );
-            SPACE.append(FRAGMENT);
-    }).catch(
-        error => {
-            console.log(error)
-        }
-    )
-})
