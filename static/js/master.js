51f757889107e4bbb19954435bcb3c3fb16c330d
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-10 16:25:42 +0300

    fixed Asides

--- static/js/master.js
+++ static/js/master.js
@@ -1,28 +1,26 @@
 class Grid {
-    static dimensions = () => {
+    static init = () => {
+        Grid.setDimensions();
+        Grid.fixChatBoxWidth();
+    }
+    static setDimensions = () => {
         document.documentElement.style.setProperty('--wh', `${window.innerHeight}px`);
         document.documentElement.style.setProperty('--ww', `${window.innerWidth}px`);
     }
-    static justifyColumnContent = () => {
-        document.querySelectorAll('main > section.column:not(#center)')
-        .forEach(
-            section => {
-                let totalWidgetsHeight = 0;
-                section.querySelectorAll('.widget')
-                .forEach(
-                    widget => {
-                        totalWidgetsHeight += widget.clientHeight;
-                });
-                if (totalWidgetsHeight/window.innerHeight < 0.75) {
-                    section.style.justifyContent = "center";
-                }
-            }
-        );
+    static fixChatBoxWidth = () => {
+        if (Template.isThere('#space')) {
+            const BOX = document.querySelector('#space-chat-box');
+            BOX.style.width = `${BOX.parentElement.offsetWidth}px`;
+        }
     }
 }
 
 
 class Aside {
+    static init = () => {
+        let asideSelector = String(localStorage.getItem('aside'));
+        if (asideSelector.includes('form')) Aside.open(asideSelector); else Aside.clear();
+    }
     static close = selector => {
         Template.blurOff();
         document.querySelector(`${selector}`).classList.remove('active');
@@ -84,6 +82,7 @@ class Template {
     }
 }
 
+
 class Form {
     static passwordToggle = passwordToggleIcon => {
         document.querySelectorAll(`#${passwordToggleIcon.parentNode.parentNode.id} input.password`)
@@ -104,6 +103,50 @@ class Message {
     }
 }
 
+
+class ConversationUI {
+    static messageUlElement = (destination, data) => {
+        let ulElement          = document.createElement('ul');
+        let mLiElement         = document.createElement('li');
+        let uLiElement         = document.createElement('li');
+        uLiElement.textContent = data.sender;
+        mLiElement.textContent = data.body;
+        ulElement.appendChild(uLiElement);
+        ulElement.appendChild(mLiElement);
+        ulElement.setAttribute(
+            'data-direction', (document.querySelector('#space').dataset.username == data.sender) ? 'out':'in'
+        );
+        ulElement.setAttribute(
+            'data-sender', data.sender
+        );
+        destination.appendChild(ulElement);
+    }
+    static messageLiElement = (destination, data) => {
+        let liElement         = document.createElement('li');
+        liElement.textContent = data.body;
+        destination.querySelector('ul:last-of-type').appendChild(liElement);
+    }
+    static messageAppend = (destination, data) => {
+        if (
+            destination.querySelector('ul:last-of-type') &&
+            destination.querySelector('ul:last-of-type').dataset.sender == data.sender
+        ) {
+            ConversationUI.messageLiElement(destination, data);
+        } else {
+            ConversationUI.messageUlElement(destination, data);
+        }
+        ConversationUI.scrollToDestination(destination);
+    }
+    static scrollToDestination = destination => {
+        destination.querySelector('ul:last-of-type').scrollIntoView(
+            {
+                block: 'start',
+                inline: 'nearest',
+                behavior:'smooth',
+            }
+        );
+    }
+}
 
 class Theme {
     static init = () => {
@@ -148,170 +191,10 @@ class Theme {
 }
 
 
-class ConversationUI {
-    static init = () => {
-        if (Template.isThere('#space')) {
-            const BOX = document.querySelector('#space-chat-box');
-            BOX.style.width = `${BOX.parentElement.offsetWidth}px`;
-        }
-    }
-    static getSpace = () => {
-        return document.querySelector('#space');
-    }
-    static messageUlElement = (destination, data) => {
-        let ulElement          = document.createElement('ul');
-        let mLiElement         = document.createElement('li');
-        let uLiElement         = document.createElement('li');
-        uLiElement.textContent = data.sender;
-        mLiElement.textContent = data.body;
-        ulElement.appendChild(uLiElement);
-        ulElement.appendChild(mLiElement);
-        ulElement.setAttribute('data-direction', (ConversationUI.getSpace().dataset.username == data.sender) ? 'out':'in');
-        ulElement.setAttribute('data-sender', data.sender);
-        destination.appendChild(ulElement);
-    }
-    static messageLiElement = (destination, data) => {
-        let liElement         = document.createElement('li');
-        liElement.textContent = data.body;
-        destination.querySelector('ul:last-of-type').appendChild(liElement);
-    }
-    static messageAppend = (destination, data) => {
-        if (
-            destination.querySelector('ul:last-of-type') &&
-            destination.querySelector('ul:last-of-type').dataset.sender == data.sender
-        ) {
-            ConversationUI.messageLiElement(destination, data);
-        } else {
-            ConversationUI.messageUlElement(destination, data);
-        }
-        ConversationUI.scrollToDestination(destination);
-    }
-    static scrollToDestination = destination => {
-        destination.querySelector('ul:last-of-type').scrollIntoView(
-            {
-                block: 'start',
-                inline: 'nearest',
-                behavior:'smooth',
-            }
-        );
-    }
-}
-
-
-class Thread {
-    static init = () => {
-        if (Template.isThere('.cards-s')) {
-            let isDown = false;
-            let startX, scrollLeft;
-            document.querySelectorAll('.card-s').forEach(
-                thread => {
-                    thread.addEventListener('mousedown', (e) => {
-                        isDown = true;
-                        startX = e.pageX - thread.offsetLeft;
-                        scrollLeft = thread.scrollLeft;
-                    });
-                    thread.addEventListener('mouseleave', () => {
-                        isDown = false;
-                    });
-                    thread.addEventListener('mouseup', () => {
-                        isDown = false;
-                    });
-                    thread.addEventListener('mousemove', (e) => {
-                        if (!isDown) return;
-                        e.preventDefault();
-                        const x = e.pageX - thread.offsetLeft;
-                        const walk = x - startX;
-                        thread.scrollLeft = scrollLeft - walk;
-                    });
-                }
-            );
-        }
-    }
-    static next = item => {
-        item.parentElement.scrollBy(
-            {
-                left: item.clientWidth,
-                behavior: 'smooth'
-            }
-        );
-    }
-    static previous = item => {
-        item.parentElement.scrollBy(
-            {
-                left: -item.clientWidth,
-                behavior: 'smooth'
-            }
-        );
-    }
-    static first = item => {
-        item.parentElement.scrollBy(
-            {
-                left: -(item.clientWidth * item.parentElement.childElementCount),
-                behavior: 'smooth'
-            }
-        );
-    }
-    static last = item => {
-        item.parentElement.scrollBy(
-            {
-                left: (item.clientWidth * item.parentElement.childElementCount),
-                behavior: 'smooth'
-            }
-        );
-    }
-    static item = (selector, index) => {
-        const thread = document.querySelector(`${selector}`);
-        thread.querySelectorAll('.card').forEach(
-            card => {
-                card.style.outlineColor = "var(--d3)";
-            }
-        )
-        thread.querySelector(`.card:nth-child(${index})`).style.outlineColor = "var(--di)";
-        thread.querySelector(`.card:nth-child(${index})`)
-        .scrollIntoView(
-            {
-                behavior: 'smooth'
-            }
-        );
-    }
-    static arrows = selector => {
-        const i = "material-symbols-outlined";
-        for (const card of document.querySelectorAll(`${selector}`)) {
-            const f = document.createDocumentFragment();
-            if (!card.nextElementSibling) {
-                const first = document.createElement('i');
-                first.setAttribute('class', i);
-                first.setAttribute('onclick', 'Thread.first(this.parentElement)');
-                first.textContent = "first_page";
-                f.append(first);
-            }
-            if (card.previousElementSibling) {
-                const previous = document.createElement('i');
-                previous.setAttribute('class', i);
-                previous.setAttribute('onclick', 'Thread.previous(this.parentElement)');
-                previous.textContent = "keyboard_arrow_left";
-                f.append(previous);
-            }
-            if (card.nextElementSibling) {
-                const next = document.createElement('i');
-                next.setAttribute('class', i);
-                next.setAttribute('onclick', 'Thread.next(this.parentElement)');
-                next.textContent = "keyboard_arrow_right";
-                f.append(next);
-            }
-            card.appendChild(f);
-        }
-    }
-}
-
-
 class Handler {
     static init = () => {
-        Grid.dimensions();
-        Grid.justifyColumnContent();
-        Aside.clear();
-        Template.blurOff();
+        Grid.init();
+        Aside.init();
         Theme.init();
-        ConversationUI.init();
     }
 }
