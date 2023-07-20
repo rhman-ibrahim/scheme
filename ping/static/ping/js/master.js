989d2798303d26286f05634f0d5b6b69ce4601eb
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-20 13:17:48 +0300

    swapped content

--- ping/static/ping/js/master.js
+++ ping/static/ping/js/master.js
@@ -1,64 +1,72 @@
-const SERIAL        = ROOM.get().serial;
-const SOCKET        = new WebSocket(`ws://${window.location.host}/ping/${SERIAL}/`);
 
-document.addEventListener(
-    'DOMContentLoaded', 
-    () => {
-        fetch(`http://${window.location.host}/dapi/${SERIAL}/`)
-        .then(
-            response => response.json()
-        )
-        .then(
-            data => {
-                const FRAGMENT = document.createDocumentFragment();
-                data.forEach(
-                    message => {
-                        ROOM.append(
-                            FRAGMENT,
-                            message
-                        )
-                    }
-                );
-                Template.append(
-                    ROOM.get().conversation,
-                    [
-                        FRAGMENT
-                    ]
-                )
-                ROOM.scroll();
+class ROOM {
+    
+    static get = () => {
+        return {
+            serial: String(document.getElementById('room').dataset.serial),
+            username: String(document.getElementById('room-input').dataset.username),
+            message: String(document.getElementById('message').value),
+            last: document.getElementById('room').querySelector('ul:last-of-type'),
+            conversation: document.querySelector('#room'),
+        };
+    }
+
+    static input = () => {
+        if (ROOM.get().message.trim().length !== 0) return true;
+        return false;
+    }
+
+    static message = () => {
+        return JSON.stringify(
+            {
+                'sender': ROOM.get().username,
+                'body': ROOM.get().message
             }
         )
-        .catch(
-            error => console.log(error)
+    }
+
+    static li = text => {
+        const li       = document.createElement('li');
+        li.textContent = String(text);
+        return li;
+    }
+
+    static ul = data => {
+        const ul = document.createElement('ul');
+        const dr = (ROOM.get().username == data.sender) ? 'outgoing':'incoming';
+        ul.setAttribute('class', dr);
+        return ul;
+    }
+
+    static appendLi = (destination, data) => {
+        destination.querySelector('ul:last-of-type').appendChild(ROOM.li(data.body));
+    }
+
+    static appendUl = (destination, data) => {
+        destination.appendChild(
+            Template.append(
+                ROOM.ul(data),
+                [
+                    ROOM.li(data.sender),
+                    ROOM.li(data.body)
+                ]
+            )
         );
     }
-)
 
-document.getElementById('sendButton').onclick = () => {
-    if (ROOM.input()) {
-        SOCKET.send(ROOM.message());
-        ROOM.clear();
-    } 
-}
-
-SOCKET.onmessage     = received => {
-    const data       = JSON.parse(received.data);
-    if (data.event === 'notify') {
-        Template.append(
-            ROOM.get().conversation,
-            [
-                ROOM.li(data.notification)
-            ]
-        );
+    static append = (destination, data) => {
+        if (destination.querySelector('ul:last-of-type') != null) {
+            if (destination.querySelector('ul:last-of-type').dataset.sender == data.sender) ROOM.appendLi(destination, data);
+        }
+        ROOM.appendUl(destination, data);
+        ROOM.scroll();
     }
-    if (data.event === 'message') {
-        ROOM.append(
-            ROOM.get().conversation,
-            data
-        );
-    }
-}
 
-SOCKET.onerror       = error => {
-    console.error('WebSocket error:', error);
+    static clear = () => {
+        document.querySelector('#message').value = '';
+    }
+
+    static scroll = () => {
+        ROOM.get().conversation.scrollTop = ROOM.get().conversation.scrollHeight;
+    }
 }
--- ping/static/ping/js/room.js
+++ ping/static/ping/js/room.js
@@ -1,71 +1,64 @@
-class ROOM {
-    
-    static get = () => {
-        return {
-            serial: String(document.getElementById('room').dataset.serial),
-            username: String(document.getElementById('room-input').dataset.username),
-            message: String(document.getElementById('message').value),
-            last: document.getElementById('room').querySelector('ul:last-of-type'),
-            conversation: document.querySelector('#room'),
-        };
-    }
+const SERIAL        = ROOM.get().serial;
+const SOCKET        = new WebSocket(`ws://${window.location.host}/ping/${SERIAL}/`);
 
-    static input = () => {
-        if (ROOM.get().message.trim().length !== 0) return true;
-        return false;
-    }
-
-    static message = () => {
-        return JSON.stringify(
-            {
-                'sender': ROOM.get().username,
-                'body': ROOM.get().message
+document.addEventListener(
+    'DOMContentLoaded', 
+    () => {
+        fetch(`http://${window.location.host}/dapi/${SERIAL}/`)
+        .then(
+            response => response.json()
+        )
+        .then(
+            data => {
+                const FRAGMENT = document.createDocumentFragment();
+                data.forEach(
+                    message => {
+                        ROOM.append(
+                            FRAGMENT,
+                            message
+                        )
+                    }
+                );
+                Template.append(
+                    ROOM.get().conversation,
+                    [
+                        FRAGMENT
+                    ]
+                )
+                ROOM.scroll();
             }
         )
-    }
-
-    static li = text => {
-        const li       = document.createElement('li');
-        li.textContent = String(text);
-        return li;
-    }
-
-    static ul = data => {
-        const ul = document.createElement('ul');
-        const dr = (ROOM.get().username == data.sender) ? 'outgoing':'incoming';
-        ul.setAttribute('class', dr);
-        return ul;
-    }
-
-    static appendLi = (destination, data) => {
-        destination.querySelector('ul:last-of-type').appendChild(ROOM.li(data.body));
-    }
-
-    static appendUl = (destination, data) => {
-        destination.appendChild(
-            Template.append(
-                ROOM.ul(data),
-                [
-                    ROOM.li(data.sender),
-                    ROOM.li(data.body)
-                ]
-            )
+        .catch(
+            error => console.log(error)
         );
     }
+)
 
-    static append = (destination, data) => {
-        if (destination.querySelector('ul:last-of-type') != null) {
-            if (destination.querySelector('ul:last-of-type').dataset.sender == data.sender) ROOM.appendLi(destination, data);
-        }
-        ROOM.appendUl(destination, data);
-        ROOM.scroll();
-    }
+document.getElementById('sendButton').onclick = () => {
+    if (ROOM.input()) {
+        SOCKET.send(ROOM.message());
+        ROOM.clear();
+    } 
+}
 
-    static clear = () => {
-        document.querySelector('#message').value = '';
+SOCKET.onmessage     = received => {
+    const data       = JSON.parse(received.data);
+    if (data.event === 'notify') {
+        Template.append(
+            ROOM.get().conversation,
+            [
+                ROOM.li(data.notification)
+            ]
+        );
     }
+    if (data.event === 'message') {
+        ROOM.append(
+            ROOM.get().conversation,
+            data
+        );
+    }
+}
 
-    static scroll = () => {
-        ROOM.get().conversation.scrollTop = ROOM.get().conversation.scrollHeight;
-    }
+SOCKET.onerror       = error => {
+    console.error('WebSocket error:', error);
 }
