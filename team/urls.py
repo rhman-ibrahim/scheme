5ec4c13a21ec5d71add3b8bcffc1c8dc89d321ac
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-27 23:10:17 +0300

    new forms handling

--- team/urls.py
+++ team/urls.py
@@ -8,9 +8,11 @@ urlpatterns = [
     path('create/', views.create, name="create"),
     path('request/<str:serial>/', views.create_request, name="request"),
     # Founder
+    path('put/', views.add_founder_friends, name="add_founder_friends"),
     path('approve/<int:user_id>/', views.approve, name="approve"),
     path('reject/<int:user_id>/', views.reject, name="reject"),
     path('remove/<int:user_id>/', views.remove, name="remove"),
+    path('transfer/', views.transfer, name="transfer"),
     # Memebers
     path('login/', views.login, name="login"),
     path('browse/', views.browse, name="browse"),
