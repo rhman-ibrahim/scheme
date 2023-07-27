f4c94868bf9a34e6d76043fe2e866a2bbd562485
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-27 23:01:48 +0300

    optimized Signal class

--- blog/static/blog/js/master.js
+++ blog/static/blog/js/master.js
@@ -1,68 +1,38 @@
 class Signal {
+
     static fields = () => {
         return {
             classification: document.getElementById('signal-classification-input'),
-            icon: document.getElementById('signal-icon-input')
+            glyph: document.getElementById('signal-glyph-input'),
+            icon: document.getElementById('signal-icon-input'),
+            body: document.getElementById('id_body')
         }
     }
-    static comment = () => {
-        Signal.fields().classification.value = 1;
-        Signal.fields().icon.value = "comment";
-        Fixed.open('#signal-form-footer');
+
+    static get = iElement => {
+        return {
+            placeholder: iElement.parentElement.parentElement.querySelector('p').textContent,
+            classification: iElement.parentElement.querySelector('b').textContent,
+            glyph: iElement.classList.item(0),
+            icon: iElement.textContent
+        }
     }
-    static criteria = () => {
-        Signal.fields().classification.value = 2;
-        Signal.fields().icon.value = "balance";
-        Fixed.open('#signal-form-footer');
+    
+    static set = iElement => {
+        return new Promise(
+            resolve => {
+                const signal = Signal.get(iElement);
+                Signal.fields().classification.setAttribute('value', signal.classification);
+                Signal.fields().body.setAttribute('placeholder',signal.placeholder);
+                Signal.fields().glyph.setAttribute('value', signal.glyph);
+                Signal.fields().icon.setAttribute('value', signal.icon);
+                resolve();
+            }
+        )
     }
-    static decision = () => {
-        Signal.fields().classification.value = 3;
-        Signal.fields().icon.value = "gavel";
-        Fixed.open('#signal-form-footer');
+
+    static form = (iElement) => {
+        Signal.set(iElement).then(Fixed.open('#signal-form-footer'));
     }
-    static hypothesis = () => {
-        Signal.fields().classification.value = 4;
-        Signal.fields().icon.value = "cognition";
-        Fixed.open('#signal-form-footer');
-    }
-    static insight = () => {
-        Signal.fields().classification.value = 5;
-        Signal.fields().icon.value = "insights";
-        Fixed.open('#signal-form-footer');
-    }
-    static metric = () => {
-        Signal.fields().classification.value = 6;
-        Signal.fields().icon.value = "monitoring";
-        Fixed.open('#signal-form-footer');
-    }
-    static observation = () => {
-        Signal.fields().classification.value = 7;
-        Signal.fields().icon.value = "tips_and_updates";
-        Fixed.open('#signal-form-footer');
-    }
-    static opportunity = () => {
-        Signal.fields().classification.value = 8;
-        Signal.fields().icon.value = "psychology";
-        Fixed.open('#signal-form-footer');
-    }
-    static problem = () => {
-        Signal.fields().classification.value = 9;
-        Signal.fields().icon.value = "warning";
-        Fixed.open('#signal-form-footer');
-    }
-    static question = () => {
-        Signal.fields().classification.value = 10;
-        Signal.fields().icon.value = "psychology_alt";
-        Fixed.open('#signal-form-footer');
-    }
-    static default = () => {
-        Signal.fields().classification.value = 11;
-        Signal.fields().icon.value = "bubble_chart";
-        Fixed.open('#signal-form-footer');
-    }
-    static test = () => {
-        Signal.fields().classification.value = 12;
-        Signal.fields().icon.value = "science";
-        Fixed.open('#signal-form-footer');
-    }
+
 }
