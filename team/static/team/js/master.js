e64d6e58d511e4c720d163be0ef3cf55e069700d
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-30 19:47:47 +0300

    deleted JS file, moved method to master JS

--- team/static/team/js/master.js
+++
@@ -1,6 +0,0 @@
-class Circle {
-    static loginForm = circleName => {
-        document.querySelector('#circle-login-form #id_name').value = circleName;
-        Fixed.open('#circle-login-form-footer');
-    }
-}
