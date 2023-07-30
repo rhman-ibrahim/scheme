5e98a4d9c1f872a43d358a21096c51771940238a
rhman <rhmanibrahimmuhammad@gmail.com>
2023-07-30 19:38:50 +0300

    check user activity

--- user/functions.py
+++ user/functions.py
@@ -1,12 +1,20 @@
 import uuid
+
 # Django
 from django.contrib.auth import authenticate, login
 from django.contrib.admin.models import CHANGE
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.db.models import Q
+
 # Helpers
 from helpers.functions import log
+
+# Team
+from team.models import Circle, CircleRequest
+
 # User
 from user.models import Account
-
 
 def create_a_guest_user(request):
     uuid_key_8 = str(uuid.uuid4())[:8]
@@ -25,5 +33,21 @@ def create_a_guest_user(request):
             request.user.id,
             request.user,
             CHANGE,
-            "signed in as a user"
+            "signed in as a guest"
         )
+
+def check_guest_activity(request):
+    if request.user.is_guest:
+        c = Circle.objects.filter(
+            Q(founder=request.user) |
+            Q(members=request.user)
+        )
+        r = CircleRequest.objects.filter(
+            Q(user=request.user) &
+            ~Q(status=0)
+        )
+        if c.exists() or r.exists():
+            messages.info(request, "as a guest you can communicate only with 1 circle")
+            return redirect("user:guest")
+    pass
+    
