98ab48f90637aed78f5617ad2daab6701da5ce73
rhman <rhmanibrahimmuhammad@gmail.com>
2023-08-24 17:28:24 +0300

    created API views for signup and signin functionalities

--- dapi/urls.py
+++ dapi/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from .views import MessageViewSet
 from team.api import SpaceViewSet
 from ping.api import RoomViewSet
+from user.api import AccountViewSet
+
 
 app_name    = "api"
 urlpatterns = [
     # Retrieve
+    path('flash/', MessageViewSet.as_view({'get':'flash'})),
+    path('signup/', AccountViewSet.as_view({'post':'signup'})),
+    path('signin/', AccountViewSet.as_view({'post':'signin'})),
     path('create/circle/', SpaceViewSet.as_view({'post':'create'})),
     path('circle/<int:pk>/info/', SpaceViewSet.as_view({'get':'info'})),
-    path('room/<str:serial>/messages/', RoomViewSet.as_view({'get':'messages'})),
-    path('flash/', MessageViewSet.as_view({'get':'flash'}))
+    path('room/<str:serial>/messages/', RoomViewSet.as_view({'get':'messages'}))
 ]
