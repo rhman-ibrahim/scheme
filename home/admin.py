e2524d04a4925e3c4254974281721dfe82f8123f
rhman <rhmanibrahimmuhammad@gmail.com>
2023-08-27 12:37:14 +0300

    removed admin Session registration

--- home/admin.py
+++ home/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from django.contrib.sessions.models import Session
-
-class SessionAdmin(admin.ModelAdmin):
-    list_display = ['session_key', 'session_data', 'expire_date']
-
-admin.site.register(Session, SessionAdmin)
