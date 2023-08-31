f5d852779465f0e8953668ee09df7bc5a2d30e5a
rhman <rhmanibrahimmuhammad@gmail.com>
2023-08-31 12:07:48 +0300

    linked default url to home urls

--- scheme/urls.py
+++ scheme/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path, include
-from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls'), name="home"),
     path('user/', include('user.urls'), name="user"),
     path('note/', include('note.urls'), name="note"),
     path('mate/', include('mate.urls'), name="mate"),
