ee8efdf8a14abc82efcc10f94d3e4d3fddd21d0a
Rhman <85110337+rhman-ibrahim@users.noreply.github.com>
2024-01-22 19:10:53 +0200

    Created start.sh

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Check if a name is provided as an argument
+if [ -z "$1" ]; then
+    echo "Please provide a name for the folder."
+    exit 1
+fi
+
+# Step 1: Receive a name
+folder_name=$1
+
+# Step 2: Create a folder with this name
+mkdir "$folder_name"
+
+# Step 3: Inside this folder, create 3 folders: [modules, dj, re]
+cd "$folder_name" || exit
+
+mkdir modules dj re
+
+# Step 4: Use git add module twice to add 2 repos as submodules [modules/dj, modules/re]
+git init
+git submodule add https://github.com/rhman-ibrahim/scheme-django modules/dj
+git submodule add https://github.com/rhman-ibrahim/scheme-react modules/re
+
+# Display success message
+echo "Directories and submodules created successfully."
