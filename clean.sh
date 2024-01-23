0f04dc64555278f36613c01a4f74137b20f83f99
rhman <rhmanibrahimmuhammad@gmail.com>
2024-01-23 17:34:04 +0200

    explict user migration, excluded venv from find

--- clean.sh
+++ clean.sh
@@ -3,11 +3,11 @@
 # Navigate to the root directory of the project (parent of 'scripts' directory)
 cd "$(dirname "$(pwd)")" || exit
 
-# Delete all 'migrations' directories
-find . -type d -name 'migrations' -exec rm -r {} +
+# Delete all 'migrations' directories excluding 'venv'
+find . -type d \( -name 'migrations' -o -name 'venv' \) -prune -exec rm -r {} +
 
-# Delete all '__pycache__' directories
-find . -type d -name '__pycache__' -exec rm -r {} +
+# Delete all '__pycache__' directories excluding 'venv'
+find . -type d \( -name '__pycache__' -o -name 'venv' \) -prune -exec rm -r {} +
 
 # Delete 'db.sqlite3' file
 find . -type f -name 'db.sqlite3' -exec rm {} +
--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python manage.py makemigrations
+python manage.py makemigrations user
 python manage.py migrate
