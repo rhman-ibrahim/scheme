9680d319edc6ba68e01f7e949fcbcb0ee4178623
rhman <rhmanibrahimmuhammad@gmail.com>
2024-01-23 13:44:44 +0200

    managed venv and dependencies

--- start.sh
+++ start.sh
@@ -1,34 +1,53 @@
 #!/bin/bash
 
-# Check if a name is provided as an argument
+# Step 01: Check if a name is provided as an argument.
+# Step 02: Create a folder with this name.
+# Step 03: Inside this folder, create 3 folders: [submodules, modules].
+# Step 04: Initialize a git repo.
+# Step 05: Use git add module twice to add scheme-django & scheme-react.
+# Step 06: Create A .gitignore file & Add subsubmodules to it.
+# Step 07: Create A Virutal Environment.
+# Step 08: Activate The Virtual Enviroment.
+# Step 09: Install Dependencies.
+# Step 10: The first commit.
+# Step 11: Display 'End of Script.' Message.
+
+# 01:
 if [ -z "$1" ]; then
     echo "Please provide a name for the folder."
     exit 1
 fi
 
-# Step 1: Receive a name
-folder_name=$1
+# 02:
+mkdir "$1" && cd "$1" || exit
 
-# Step 2: Create a folder with this name
-mkdir "$folder_name"
+# 03: Inside modules create [dj, re].
+mkdir submodules modules modules/dj modules/re
 
-# Step 3: Inside this folder, create 3 folders: [modules, dj, re]
-cd "$folder_name" || exit
-
-mkdir modules dj re
-
-# Step 4: Use git add module twice to add 2 repos as submodules [modules/dj, modules/re]
+# 04:
 git init
-git submodule add https://github.com/rhman-ibrahim/scheme-django modules/dj
-git submodule add https://github.com/rhman-ibrahim/scheme-react modules/re
 
-# Step 5: Create a .gitignore file and add submodules to it
-echo "/modules/dj" >> .gitignore
-echo "/modules/re" >> .gitignore
+# 05:
+git submodule add https://github.com/rhman-ibrahim/scheme-django submodules/dj
+git submodule add https://github.com/rhman-ibrahim/scheme-react submodules/re
 
-# Commit the changes
+# 06:
+echo "/submodules/dj" >> .gitignore
+echo "/submodules/re" >> .gitignore
+
+# 07:
+python -m venv venv
+
+# 08:Set PYTHONPATH after activation.
+export PYTHONPATH="$PWD/venv/lib/python3.10/site-packages"
+source venv/bin/activate
+
+# 09:
+pip install -r submodules/dj/requirements.txt
+
+# 10:
 git add .
-git commit -m "Initial commit with submodules (added to the ignore file)."
+git commit -m "Initial commit with subsubmodules (added to the ignore file)."
 
-# Display success message
-echo "Directories and submodules created successfully."
+# 11:
+echo "End Of Script."
