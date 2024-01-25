a0ef933c4b09afd774f122a085fcf8ef8f2781e2
rhman <rhmanibrahimmuhammad@gmail.com>
2024-01-25 18:41:23 +0200

    fixed paths in link

--- link.sh
+++ link.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # Define root path.
-root_path="../$PWD"
+cd ../
 
 # Define the source & target paths.
-source_path="$root_path/modules/dj"
-target_path="$root_path/submodules/dj/apps"
+source_path="$PWD/modules/dj"
+target_path="$PWD/submodules/dj/apps"
 
 # Iterate through each folder in the source path
 for folder in "$source_path"/*; do
-
     # Extract folder name
     folder_name=$(basename "$folder")
 
@@ -18,10 +17,10 @@ for folder in "$source_path"/*; do
         # Check if the symbolic link exists in the target path
         if [ ! -L "$target_path/$folder_name" ]; then
             # Create symbolic link
-            ln -s "$folder" "$target_path/$folder_name"
+            ln -s "$source_path/$folder_name" "$target_path/$folder_name"
             echo "Created symbolic link for folder: $folder_name"
         fi
     fi
 done
 
-echo "Symbolic link check and creation completed."
+# echo "Symbolic link check and creation completed."
