9fb920aa2d3ffd3db027f7a21815d7dc27fec6f6
rhman <rhmanibrahimmuhammad@gmail.com>
2024-01-26 12:22:48 +0200

    added react run commands

--- run.sh
+++ run.sh
@@ -10,8 +10,10 @@ fi
 
 if [ "$1" = "t" ]; then
     gnome-terminal --tab --title "Django" -- bash -c "python $manage_utility_path runserver 0.0.0.0:8000"
+    gnome-terminal --tab --title "React" -- bash -c "npm run dev"
 elif [ "$1" = "w" ]; then
-    gnome-terminal --window --working-directory "$PWD" --title "Django" -- bash -c "python $manage_utility_path runserver 0.0.0.0:8000"
+    gnome-terminal --window --title "Django" -- bash -c "python $manage_utility_path runserver 0.0.0.0:8000"
+    gnome-terminal --window --title "React" -- bash -c "npm run dev"
 else
     echo "Invalid argument. Please provide 't' for tab or 'w' for window."
     exit 1
