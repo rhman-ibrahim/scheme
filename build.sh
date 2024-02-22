7ec71a6932740f97d52095493655b319e1ec1b29
rhman <rhmanibrahimmuhammad@gmail.com>
2024-02-22 17:26:17 +0200

    updated paths, restructured steps

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Step 01: Check if a name is provided & create the directories if so.
-# Step 02: Initialize a git repo & add base (sch-py, sch-js, sch-sh & sch-cf).
-# Step 03: Delete configuration files and entry point from sch-py & sch-js.
-# Step 04: Create A .gitignore file.
-# Step 05: Create A Virutal Environment, set both & activate.
-# Step 06: Install Dependencies.
+# Step 02: Initialize a git repo & add base (scheme.api, scheme.ui, scheme.bash).
+# Step 03: Manage configuration files & entry point.
+# Step 04: Create the Python virtual environment & set NPM prefix.
+# Step 05: Install Dependencies.
+# Step 06: Create A .gitignore file.
 # Step 07: The first commit.
 # Step 08: Open the project.
 
@@ -15,30 +15,37 @@ if [ -z "$1" ]; then
     exit 1
 else
     mkdir "$1" && cd "$1"
-    mkdir base modules
-    mkdir modules/django modules/react
+    mkdir submodules modules
+    mkdir modules/api modules/ui
 fi
 
 # 02:
 git init
-git submodule add https://github.com/rhman-ibrahim/scheme.django base/django
-git submodule add https://github.com/rhman-ibrahim/scheme.react base/react
-git submodule add https://github.com/rhman-ibrahim/scheme.bash scripts
-git submodule add https://github.com/rhman-ibrahim/scheme.config configs
+git submodule add https://github.com/rhman-ibrahim/scheme.bash submodules/scripts
+git submodule add https://github.com/rhman-ibrahim/scheme.django submodules/api
+git submodule add https://github.com/rhman-ibrahim/scheme.react submodules/ui
 
 # 03:
-mv configs/* .
-mv base/django/requirements.txt .
-mv base/react/package.json .
-mv base/react/package-lock.json .
-mv base/react/.eslintrc.cjs .
-mv base/react/public .
-rm base/react/index.html
-rm base/react/vite.config.js
-rm base/react/jsconfig.json
-rm -rf configs
+mv submodules/api/requirements.txt .
+mv submodules/ui/package.json .
+mv submodules/ui/package-lock.json .
+mv submodules/ui/.eslintrc.cjs .
+mv submodules/ui/public .
+rm submodules/ui/index.html
+rm submodules/ui/vite.config.js
+rm submodules/ui/jsconfig.json
 
 # 04:
+python -m venv venv
+export PYTHONPATH="$PWD/venv/lib/python3.10/site-packages"
+source venv/bin/activate
+npm config set prefix .
+
+# 05:
+pip install -r requirements.txt
+npm install
+
+# 06:
 echo "\
 db.sqlite3
 **/migrations/
@@ -50,21 +57,9 @@ venv/
 node_modules/
 " >> .gitignore
 
-# 05:
-python -m venv venv
-export PYTHONPATH="$PWD/venv/lib/python3.10/site-packages"
-source venv/bin/activate
-npm config set prefix .
-
-# 06:
-pip install -r requirements.txt
-npm install
-
 # 07:
-git submodule deinit -f configs
 git rm --cached -rf scripts/
 git rm --cached -rf base/
-git rm --cached -rf configs/
 git add .
 git commit -m "Initial commit"
 
